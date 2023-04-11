7b241742bd80f5d608caceeae000bdf973bdf670
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-04-11 14:00:00 +0200

    Optimize storage for off-chain data

--- cryptocurrency_payment_gateways.sol
+++ cryptocurrency_payment_gateways.sol
@@ -1,3 +1,4 @@
 // Add support for decentralized content distribution
 // Add batch processing for transactions
 // Integrate with decentralized identity providers
+// Refactor governance token logic
