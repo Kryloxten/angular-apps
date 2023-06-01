834811319d5c4ee1badcdd4a9b19463d56eaa8cd
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-06-01 14:00:00 +0200

    Fix interoperability issues with other blockchains

--- token_vesting_schedule.go
+++ token_vesting_schedule.go
@@ -1,3 +1,4 @@
 // Add Layer-2 Ethereum scaling solutions
 // Add support for decentralized credit scoring
 // Optimize decentralized oracle performance
+// Refactor incentive structure in DeFi protocol
