b44f8fa55a78c1b65d6bc1101e7aca8f030df50e
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-08-06 14:00:02 +0200

    Refactor contract state machine design

--- scalable_blockchain_infrastructure.go
+++ scalable_blockchain_infrastructure.go
@@ -2,3 +2,4 @@
 // Fix interoperability issues with other blockchains
 // Add batch processing for transactions
 // Add support for zk-SNARKs in smart contracts
+// Fix compatibility issues with major wallets
