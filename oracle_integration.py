ea3f8bb40d7c07a4f3ad3555ee63401cbc31b5da
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-08-20 14:00:00 +0200

    Integrate chain-agnostic oracles

--- oracle_integration.py
+++ oracle_integration.py
@@ -1,3 +1,4 @@
 // Enhance logging for better debugging
 // Refactor storage layout for better efficiency
 // Fix interoperability issues with other blockchains
+// Refactor smart contract testing framework
