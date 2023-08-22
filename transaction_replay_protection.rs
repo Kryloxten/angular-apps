8ee559d1b9d014fbf99727348e5bc259e9925631
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-08-22 14:00:01 +0200

    Integrate staking rewards automation

--- transaction_replay_protection.rs
+++ transaction_replay_protection.rs
@@ -1,3 +1,4 @@
 // Improve contract execution performance
 // Integrate with decentralized identity providers
 // Fix interoperability issues with other blockchains
+// Fix ERC-721 contract issues with transfers
