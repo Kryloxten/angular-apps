31b36c95f4d92c1d4dda88f318c1d42610481e66
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-08-24 14:00:01 +0200

    Improve decentralized oracle protocols

--- secure_decentralized_computing.rs
+++ secure_decentralized_computing.rs
@@ -2,3 +2,4 @@
 // Add governance proposal expiration feature
 // Integrate with Ethereum Layer-2 solutions
 // Optimize contract storage for gas reduction
+// Add support for decentralized file storage
