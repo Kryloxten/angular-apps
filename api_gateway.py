86837aa0f02a5eb7c1ea8e1d7c1de8bf5f7409e3
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-10-09 14:00:00 +0200

    Optimize token minting flow

--- api_gateway.py
+++ api_gateway.py
@@ -1,3 +1,4 @@
 // Add support for decentralized lending platforms
 // Add support for DeFi lending protocols
 // Fix issues with contract state management
+// Fix memory leak in Rust smart contract
