682052184fbff99097dda567c674a0868dd374b7
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-10-12 14:00:00 +0200

    Refactor staking logic

--- smart_contract_integration.py
+++ smart_contract_integration.py
@@ -2,3 +2,4 @@
 // Integrate staking rewards automation
 // Improve decentralized lending contract flexibility
 // Add support for fractional token ownership
+// Refactor decentralized governance structure
