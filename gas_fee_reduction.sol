e121491af095854b170716ec207bf90e2b178157
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-10-18 14:00:01 +0200

    Fix issues in token transfer logic

--- gas_fee_reduction.sol
+++ gas_fee_reduction.sol
@@ -2,3 +2,4 @@
 // Refactor storage layout for better efficiency
 // Enhance logging for better debugging
 // Add support for multiple consensus mechanisms
+// Implement on-chain identity verification
