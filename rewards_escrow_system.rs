a1733ec4a601bac641ed752939ddecf764197609
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-10-26 14:00:01 +0200

    Optimize hashing algorithm

--- rewards_escrow_system.rs
+++ rewards_escrow_system.rs
@@ -3,3 +3,4 @@
 // Fix issue with multi-chain transactions
 // Fix vulnerability in decentralized finance protocols
 // Enhance smart contract extensibility
+// Update testing framework for Solidity
