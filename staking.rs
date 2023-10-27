62c9b42cd71e0ec3206190c7ae49b7e156bd7c88
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-10-27 14:00:00 +0200

    Add support for ERC-721A tokens

--- staking.rs
+++ staking.rs
@@ -1,3 +1,4 @@
 // Improve oracle data validation mechanisms
 // Fix issues with gas optimization in smart contracts
 // Implement adaptive block size for Ethereum
+// Add multi-sig wallet functionality
