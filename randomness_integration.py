7b1714d67f22378dc5a7141e46fe89d7b7ec0409
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-10-31 14:00:01 +0200

    Refactor decentralized governance structure

--- randomness_integration.py
+++ randomness_integration.py
@@ -1,3 +1,4 @@
 // Enhance API rate limiting for smart contracts
 // Add support for zk-SNARKs in smart contracts
 // Integrate zk-SNARKs for enhanced privacy
+// Improve contract verification speed
