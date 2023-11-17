48ca4db20c42df5b7cf598b1076d31fb34b2a800
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-11-17 14:00:00 +0200

    Refactor contract to use EIP-712 signatures

--- transaction_history_analysis.rs
+++ transaction_history_analysis.rs
@@ -2,3 +2,4 @@
 // Add support for decentralized lending platforms
 // Add support for decentralized file storage
 // Integrate with decentralized identity providers
+// Add support for decentralized credit scoring
