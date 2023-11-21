52437fdc8cc52a1637b8c58c70390bd982922615
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-11-21 14:00:01 +0200

    Refactor governance token logic

--- staking.rs
+++ staking.rs
@@ -2,3 +2,4 @@
 // Fix issues with gas optimization in smart contracts
 // Implement adaptive block size for Ethereum
 // Add multi-sig wallet functionality
+// Fix deserialization bug in Haskell code
