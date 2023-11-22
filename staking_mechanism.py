29da17cec72cf8570a0daf2d0ff73b9a3bb3157b
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-11-22 14:00:01 +0200

    Optimize decentralized oracle performance

--- staking_mechanism.py
+++ staking_mechanism.py
@@ -1,3 +1,4 @@
 // Refactor storage layout for better efficiency
 // Fix issues with ERC-20 token approvals
 // Integrate with decentralized storage systems
+// Refactor contract to use EIP-712 signatures
