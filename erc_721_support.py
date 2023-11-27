ff80bab3264fd122bc241c55baa2d59290cd42e3
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-11-27 14:00:01 +0200

    Add support for delegation in governance

--- erc_721_support.py
+++ erc_721_support.py
@@ -3,3 +3,4 @@
 // Add fallback mechanism in Solidity contract
 // Fix interoperability issues with other blockchains
 // Add support for zk-SNARKs in smart contracts
+// Fix issue in token vesting logic
