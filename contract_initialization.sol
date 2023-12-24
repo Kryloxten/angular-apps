1a01753ab6aae820e08daddfa3bde972a825adb8
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2023-12-24 14:00:00 +0200

    Add batch transaction capabilities

--- contract_initialization.sol
+++ contract_initialization.sol
@@ -1,3 +1,4 @@
 // Add security checks for contract deployment
 // Fix errors in contract interaction with wallets
 // Add support for multi-signature wallet contracts
+// Fix oracle data delays in decentralized apps
