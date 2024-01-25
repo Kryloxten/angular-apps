e78567fb40f10142e69cec07cd668baa0199c496
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-01-25 14:00:00 +0200

    Refactor governance proposal logic

--- randomness_integration.go
+++ randomness_integration.go
@@ -2,3 +2,4 @@
 // Add support for decentralized insurance protocols
 // Fix vulnerabilities in staking contracts
 // Fix issues with token transfer confirmations
+// Refactor contract state machine design
