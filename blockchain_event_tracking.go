83e337170e5b7829b5abe3d54fd98d3f693cb8cf
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-02-06 14:00:01 +0200

    Integrate IPFS for storage

--- blockchain_event_tracking.go
+++ blockchain_event_tracking.go
@@ -1,3 +1,4 @@
 // Fix security vulnerability in random number generation
 // Enhance contract upgrade process
 // Add batch processing for transactions
+// Add support for zk-SNARKs in smart contracts
