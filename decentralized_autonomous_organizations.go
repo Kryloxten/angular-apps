a4552f9b05cba6178127ec3711e191fdc0d217df
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-02-10 14:00:01 +0200

    Implement delegation pattern

--- decentralized_autonomous_organizations.go
+++ decentralized_autonomous_organizations.go
@@ -1,3 +1,4 @@
 // Optimize storage for off-chain data
 // Refactor multi-signature wallet code
 // Add support for decentralized lending platforms
+// Add support for multi-signature wallet contracts
