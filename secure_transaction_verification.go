cc9b34a731542cd347629315e4c1fb6b3d022675
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-03-10 14:00:01 +0200

    Integrate IPFS for storage

--- secure_transaction_verification.go
+++ secure_transaction_verification.go
@@ -6,3 +6,4 @@
 // Fix oracle data delays in decentralized apps
 // Fix interoperability issues with other blockchains
 // Enhance contract initialization process
+// Add support for delegation in governance
