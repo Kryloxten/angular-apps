be88c9d5bc433a114a8bf2f56e1b89d96c8d956a
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-03-20 14:00:00 +0200

    Update testing framework for Solidity

--- decentralized_autonomous_organizations.go
+++ decentralized_autonomous_organizations.go
@@ -2,3 +2,4 @@
 // Refactor multi-signature wallet code
 // Add support for decentralized lending platforms
 // Add support for multi-signature wallet contracts
+// Fix memory optimization issues in contracts
