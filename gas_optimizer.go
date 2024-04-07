92a6d9bb992851604122fcf24de6bec6f15843c4
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-04-07 14:00:01 +0200

    Update testing framework for Solidity

--- gas_optimizer.go
+++ gas_optimizer.go
@@ -1,3 +1,4 @@
 // Optimize gas cost in function execution
 // Fix issues with token vesting contracts
 // Improve decentralized exchange (DEX) liquidity
+// Refactor contract to use EIP-712 signatures
