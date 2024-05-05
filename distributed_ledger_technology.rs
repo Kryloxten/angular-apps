5a65e8702b56c12cb72bcbf8a021534daff762a1
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-05-05 14:00:01 +0200

    Optimize gas cost in function execution

--- distributed_ledger_technology.rs
+++ distributed_ledger_technology.rs
@@ -4,3 +4,4 @@
 // Implement time-locked contract execution
 // Implement reward distribution feature
 // Improve scalability for smart contract platforms
+// Refactor contract to use EIP-712 signatures
