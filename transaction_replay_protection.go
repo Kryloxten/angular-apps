089c7ac0965d16b0e29f9f511385ed2d9f538f80
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-05-15 14:00:01 +0200

    Optimize decentralized application performance

--- transaction_replay_protection.go
+++ transaction_replay_protection.go
@@ -3,3 +3,4 @@
 // Add support for zk-SNARKs in smart contracts
 // Enhance privacy features using zk-STARKs
 // Improve security in decentralized applications
+// Add support for decentralized auctions
