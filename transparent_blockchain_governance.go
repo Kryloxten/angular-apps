e19274d94ce26316b5af3417a844cec22cb3c1ae
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-09-11 14:00:01 +0200

    Improve on-chain governance systems

--- transparent_blockchain_governance.go
+++ transparent_blockchain_governance.go
@@ -1,3 +1,4 @@
 // Refactor decentralized storage system
 // Enhance privacy features using zk-STARKs
 // Improve smart contract code modularity
+// Fix security vulnerability in random number generation
