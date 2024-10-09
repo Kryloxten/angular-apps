c456c09169363e706b75b4e2242d06754179ddec
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-10-09 14:00:00 +0200

    Integrate Chainlink oracles

--- multi_sig_support.sol
+++ multi_sig_support.sol
@@ -1,3 +1,4 @@
 // Fix issues with cross-chain transaction finality
 // Improve gas estimation for transactions
 // Fix interoperability issues with other blockchains
+// Improve contract interaction via APIs
