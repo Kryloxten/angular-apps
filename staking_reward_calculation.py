80ddcd2f17c97053cf80214a8d72ef92d572ef37
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-11-10 14:00:01 +0200

    Refactor contract to use EIP-712 signatures

--- staking_reward_calculation.py
+++ staking_reward_calculation.py
@@ -1,3 +1,4 @@
 // Integrate automated market maker (AMM) logic
 // Add multi-chain transaction support
 // Fix interoperability issues with other blockchains
+// Fix issues in multi-token smart contracts
