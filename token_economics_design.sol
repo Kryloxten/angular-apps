414e763fd21f561b647968063288f7b12e7282f9
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-11-13 14:00:01 +0200

    Fix issues with token vesting contracts

--- token_economics_design.sol
+++ token_economics_design.sol
@@ -2,3 +2,4 @@
 // Fix issues with cross-chain transaction finality
 // Add support for multiple consensus mechanisms
 // Fix data consistency issues in smart contracts
+// Implement liquidity pool staking mechanism
