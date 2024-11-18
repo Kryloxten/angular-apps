747ebe2ba7b87fa83378bb58c28b563bdbeaa82d
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-11-18 14:00:00 +0200

    Refactor cross-chain communication layer

--- decentralized_exchange.go
+++ decentralized_exchange.go
@@ -4,3 +4,4 @@
 // Fix ERC-1155 contract interactions
 // Fix data consistency issues in smart contracts
 // Improve ERC-721 token metadata functionality
+// Fix interoperability issues with other blockchains
