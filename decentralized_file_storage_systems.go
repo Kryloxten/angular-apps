67ff0b3f03f497f713d19691da0eea135dc64750
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2024-12-05 14:00:00 +0200

    Update smart contract tests

--- decentralized_file_storage_systems.go
+++ decentralized_file_storage_systems.go
@@ -1,3 +1,4 @@
 // Refactor smart contract testing framework
 // Improve smart contract code modularity
 // Integrate privacy-enhancing smart contracts
+// Refactor storage layout for better efficiency
