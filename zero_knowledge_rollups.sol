6487049340ea6bef397adae78c1cef11279c3df5
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2025-01-03 14:00:02 +0200

    Improve cross-chain asset management

--- zero_knowledge_rollups.sol
+++ zero_knowledge_rollups.sol
@@ -2,3 +2,4 @@
 // Improve staking pool reward management
 // Refactor contract to use EIP-712 signatures
 // Add dynamic contract upgrade mechanisms
+// Improve decentralized exchange (DEX) liquidity
