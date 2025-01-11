0ebf854c2231f09379cf88d2f873222cd75b92ec
Kryloxten <155024674+Kryloxten@users.noreply.github.com>
2025-01-11 14:00:02 +0200

    Improve DApp transaction flow

--- token_burning.py
+++ token_burning.py
@@ -5,3 +5,4 @@
 // Fix bugs in NFT metadata handling
 // Add support for decentralized credit scoring
 // Improve gas estimation for transactions
+// Fix issues with gas optimization in smart contracts
